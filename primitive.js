10c9caee6a65bc808537040331845052c6626590
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-03 10:00:17 +0600

    primitive

---
+++ primitive.js
@@ -0,0 +1,8 @@
+let data = 23;
+console.log(typeof data);
+data = 'Minhajul';
+console.log(typeof data);
+data = [12, 23, 34, 34, 43];
+console.log(typeof data);
+data = true;
+console.log(typeof data);
