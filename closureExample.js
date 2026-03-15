2bc1718e7beff50b4a5ac121ae6f98dd0d481ba5
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-15 19:15:05 +0600

    closure example

--- closureExample.js
+++ closureExample.js
@@ -21,3 +21,5 @@ account.withdraw(200);  // Balance: 1300
 account.deposit(340);
 account.deposit(34434);
 account.withdraw(43435);
+
+// In this example, the `bankAccount` function creates a closure that encapsulates the `balance` variable. The returned object has two methods, `deposit` and `withdraw`, which can access and modify the `balance` variable even after the `bankAccount` function has finished executing. This allows us to maintain the state of the bank account across multiple operations.
